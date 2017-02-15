b4a6f38e2e96c08c4c591688977b62fd8a46edf4
shimizuyudai <shimizu.yudai.0730@gmail.com>
2017-02-16 02:40:02 +0900

    first_commit

---
+++ src/ofxPlaneWarp.cpp
@@ -0,0 +1,100 @@
+#include "ofxPlaneWarp.h"
+
+void ofxPlaneWarp::draw(ofTexture & texture)
+{
+	texture.bind();
+	mesh.draw();
+	texture.unbind();
+	if (isWarpMode) {
+		ofSetColor(0, 255, 0,127);
+		for (auto i = 0; i < cornerPoints.size(); i++) {
+			ofDrawCircle(cornerPoints[i][0],cornerPoints[i][1],touchDistThreashold);
+		}
+	}
+
+}
+
+void ofxPlaneWarp::setup(float x, float y, float w, float h, int wSeg, int hSeg)
+{
+	auto startX = x;
+	auto startY = y;
+
+	for (auto i = 0; i < hSeg; i++) {
+		for (auto j = 0; j < wSeg; j++) {
+			auto vertex = ofVec2f(startX + (w / (float)wSeg)*(float)j, startY + (h / (float)hSeg)*(float)i);
+			mesh.addVertex(vertex);
+			basePoints.push_back(cv::Vec2f(vertex.x,vertex.y));
+			auto uv = ofVec2f((float)j/(float)wSeg,(float)i/ (float)hSeg);
+			mesh.addTexCoord(uv);
+		}
+	}
+
+	for (auto i = 0; i < hSeg; i++) {
+		for (auto j = 0; j < wSeg; j++) {
+			if (j + 1 < wSeg && i + 1 < hSeg) {
+				auto index = j + (i*wSeg);
+				mesh.addIndex(index);
+				mesh.addIndex(index + wSeg);
+				mesh.addIndex(index + 1);
+				mesh.addIndex(index + 1);
+				mesh.addIndex(index + wSeg);
+				mesh.addIndex(index + wSeg + 1);
+			}
+		}
+	}
+
+	baseCornerPoints.push_back(cv::Vec2f(mesh.getVertex(0).x, mesh.getVertex(0).y));
+	baseCornerPoints.push_back(cv::Vec2f(mesh.getVertex(wSeg*(hSeg - 1)).x, mesh.getVertex(wSeg*(hSeg - 1)).y));
+	baseCornerPoints.push_back(cv::Vec2f(mesh.getVertex((wSeg*hSeg) - 1).x, mesh.getVertex((wSeg*hSeg) - 1).y));
+	baseCornerPoints.push_back(cv::Vec2f(mesh.getVertex(wSeg - 1).x, mesh.getVertex(wSeg - 1).y));
+
+	cornerPoints.push_back(cv::Vec2f(mesh.getVertex(0).x, mesh.getVertex(0).y));
+	cornerPoints.push_back(cv::Vec2f(mesh.getVertex(wSeg*(hSeg - 1)).x, mesh.getVertex(wSeg*(hSeg - 1)).y));
+	cornerPoints.push_back(cv::Vec2f(mesh.getVertex((wSeg*hSeg) - 1).x, mesh.getVertex((wSeg*hSeg) - 1).y));
+	cornerPoints.push_back(cv::Vec2f(mesh.getVertex(wSeg - 1).x, mesh.getVertex(wSeg - 1).y));
+}
+
+void ofxPlaneWarp::update()
+{
+	h = cv::findHomography(baseCornerPoints, cornerPoints);
+	vector<cv::Vec2f> dst;
+	cv::perspectiveTransform(basePoints,dst,h);
+	for (auto i = 0; i < dst.size(); i++) {
+		mesh.setVertex(i,ofVec2f(dst[i][0], dst[i][1]));
+	}
+}
+
+void ofxPlaneWarp::warpMode(bool value)
+{
+	isWarpMode = value;
+}
+
+void ofxPlaneWarp::mouseDragged(int x, int y, int button)
+{
+	cornerPoints[selectedPointIndex][0] = x;
+	cornerPoints[selectedPointIndex][1] = y;
+
+	update();
+}
+
+void ofxPlaneWarp::mousePressed(int x, int y, int button)
+{
+	if (isWarpMode) {
+		for (auto i = 0; i < cornerPoints.size(); i++) {
+			if (ofDist(x, y, cornerPoints[i][0], cornerPoints[i][1]) < touchDistThreashold) {
+				selectedPointIndex = i;
+				break;
+			}
+		}
+	}
+}
+
+void ofxPlaneWarp::mouseReleased(int x, int y, int button)
+{
+	update();
+}
+
+void ofxPlaneWarp::drawWireframe()
+{
+	mesh.drawWireframe();
+}
---
+++ src/ofxPlaneWarp.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxOpenCv.h"
+#include "opencv.hpp"
+
+class ofxPlaneWarp {
+public:
+	void draw(ofTexture& texture);
+	void drawWireframe();
+	void setup(float x,float y,float w,float h,int wSeg,int hSeg);
+	void update();
+	void warpMode(bool value);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+
+private:
+
+	ofMesh mesh;
+	float touchDistThreashold = 10;
+	vector<cv::Vec2f> baseCornerPoints;
+	vector<cv::Vec2f> basePoints;
+	vector<cv::Vec2f> cornerPoints;
+	
+	cv::Mat h;
+	bool isWarpMode;
+	int selectedPointIndex;
+};
